7b61b3e84ff7ae08b4e99c6114d1a1871926895c
deenrookie <deenrookie@gmail.com>
2021-09-01 01:27:44 +0800

    提交表单

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ApiInput />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApiInput from './components/ApiInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ApiInput
   }
 }
 </script>
@@ -21,8 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 15vh;
+  margin-left: 10vh;
 }
 </style>
